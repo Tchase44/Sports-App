074b67f8b78069d56906039e321fab7aac4eb4a1
Tarik Kazanci <tarikkzc@gmail.com>
2017-06-23 15:40:16 -0400

    nba data completed

---
+++ nba.js
@@ -0,0 +1,173 @@
+let nbaData = [{
+		"name": "Atlanta Hawks",
+		"sport": "Basketball",
+		"city": "Atlanta",
+		"logo_url": "http://content.sportslogos.net/logos/6/220/full/9168_atlanta_hawks-primary-2016.png"
+	},
+	{
+		"name": "Boston Celtics",
+		"sport": "Basketball",
+		"city": "Boston",
+		"logo_url": "http://content.sportslogos.net/logos/6/213/full/slhg02hbef3j1ov4lsnwyol5o.png"
+	},
+	{
+		"name": "Brooklyn Nets",
+		"sport": "Basketball",
+		"city": "Brooklyn",
+		"logo_url": "http://content.sportslogos.net/logos/6/3786/full/137_brooklyn-nets-primary-2013.png"
+	},
+	{
+		"name": "Charlotte Hornets",
+		"sport": "Basketball",
+		"city": "Charlotte",
+		"logo_url": "http://content.sportslogos.net/logos/6/5120/full/1926_charlotte__hornets_-primary-2015.png"
+	},
+	{
+		"name": "Chicago Bulls",
+		"sport": "Basketball",
+		"city": "Chicago",
+		"logo_url": "http://content.sportslogos.net/logos/6/221/full/hj3gmh82w9hffmeh3fjm5h874.png"
+	},
+	{
+		"name": "Cleveland Cavaliers",
+		"sport": "Basketball",
+		"city": "Cleveland",
+		"logo_url": "http://content.sportslogos.net/logos/6/222/full/4510_cleveland_cavaliers-alternate-2018.png"
+	},
+	{
+		"name": "Dallas Mavericks",
+		"sport": "Basketball",
+		"city": "Dallas",
+		"logo_url": "http://content.sportslogos.net/logos/6/228/full/ifk08eam05rwxr3yhol3whdcm.png"
+	},
+	{
+		"name": "Denver Nuggets",
+		"sport": "Basketball",
+		"city": "Denver",
+		"logo_url": "http://content.sportslogos.net/logos/6/229/full/7192_denver_nuggets-primary_on_dark-2009.png"
+	},
+	{
+		"name": "Detroit Pistons",
+		"sport": "Basketball",
+		"city": "Detroit",
+		"logo_url": "http://content.sportslogos.net/logos/6/223/full/2164_detroit_pistons-primary-2018.png"
+	},
+	{
+		"name": "Golden State Warriors",
+		"sport": "Basketball",
+		"city": "Golden State",
+		"logo_url": "http://content.sportslogos.net/logos/6/235/full/qhhir6fj8zp30f33s7sfb4yw0.png"
+	},
+	{
+		"name": "Houston Rockets",
+		"sport": "Basketball",
+		"city": "Houston",
+		"logo_url": "http://content.sportslogos.net/logos/6/230/full/2365_houston_rockets-primary_on_dark-2004.png"
+	},
+	{
+		"name": "Indiana Pacers",
+		"sport": "Basketball",
+		"city": "Indiana",
+		"logo_url": "http://content.sportslogos.net/logos/6/224/full/3083.gif"
+	},
+	{
+		"name": "Los Angeles Clippers",
+		"sport": "Basketball",
+		"city": "Los Angeles",
+		"logo_url": "http://content.sportslogos.net/logos/6/236/full/5462_los_angeles_clippers-primary-2016.png"
+	},
+	{
+		"name": "Los Angeles Lakers",
+		"sport": "Basketball",
+		"city": "Los Angeles",
+		"logo_url": "http://content.sportslogos.net/logos/6/237/full/uig7aiht8jnpl1szbi57zzlsh.png"
+	},
+	{
+		"name": "Memphis Grizzlies",
+		"sport": "Basketball",
+		"city": "Memphis",
+		"logo_url": "http://content.sportslogos.net/logos/6/231/full/793.gif"
+	},
+	{
+		"name": "Miami Heat",
+		"sport": "Basketball",
+		"city": "Miami",
+		"logo_url": "http://content.sportslogos.net/logos/6/214/full/burm5gh2wvjti3xhei5h16k8e.gif"
+	},
+	{
+		"name": "Milwaukee Bucks",
+		"sport": "Basketball",
+		"city": "Milwaukee",
+		"logo_url": "http://content.sportslogos.net/logos/6/225/full/8275_milwaukee_bucks-primary-2016.png"
+	},
+	{
+		"name": "Minnesota Timberwolves",
+		"sport": "Basketball",
+		"city": "Minnesota",
+		"logo_url": "http://content.sportslogos.net/logos/6/232/full/9669_minnesota_timberwolves-primary-2018.png"
+	},
+	{
+		"name": "New Orleans Pelicans",
+		"sport": "Basketball",
+		"city": "New Orleans",
+		"logo_url": "http://content.sportslogos.net/logos/6/4962/full/2681_new_orleans_pelicans-primary-2014.png"
+	},
+	{
+		"name": "New York Knicks",
+		"sport": "Basketball",
+		"city": "New York",
+		"logo_url": "http://content.sportslogos.net/logos/6/216/full/2nn48xofg0hms8k326cqdmuis.gif"
+	},
+	{
+		"name": "Oklahoma City Thunder",
+		"sport": "Basketball",
+		"city": "Oklahoma City",
+		"logo_url": "http://content.sportslogos.net/logos/6/2687/full/khmovcnezy06c3nm05ccn0oj2.png"
+	}, {
+		"name": "Orlando Magic",
+		"sport": "Basketball",
+		"city": "Orlando",
+		"logo_url": "http://content.sportslogos.net/logos/6/217/full/wd9ic7qafgfb0yxs7tem7n5g4.gif"
+	}, {
+		"name": "Philadelphia 76ers",
+		"sport": "Basketball",
+		"city": "Philadelphia",
+		"logo_url": "http://content.sportslogos.net/logos/6/218/full/7034_philadelphia_76ers-primary-2016.png"
+	}, {
+		"name": "Phoenix Suns",
+		"sport": "Basketball",
+		"city": "Phoenix",
+		"logo_url": "http://content.sportslogos.net/logos/6/238/full/4370_phoenix_suns-primary-2014.png"
+	}, {
+		"name": "Portland Trail Blazers",
+		"sport": "Basketball",
+		"city": "Portland",
+		"logo_url": "http://content.sportslogos.net/logos/6/239/full/9725_portland_trail_blazers-primary-2018.png"
+	}, {
+		"name": "Sacramento Kings",
+		"sport": "Basketball",
+		"city": "Sacramento",
+		"logo_url": "http://content.sportslogos.net/logos/6/240/full/4043_sacramento_kings-primary-2017.png"
+	}, {
+		"name": "San Antonio Spurs",
+		"sport": "Basketball",
+		"city": "San Antonio",
+		"logo_url": "http://content.sportslogos.net/logos/6/233/full/827.png"
+	}, {
+		"name": "Toronto Raptors",
+		"sport": "Basketball",
+		"city": "Toronto",
+		"logo_url": "http://content.sportslogos.net/logos/6/227/full/4578_toronto_raptors-primary-2016.png"
+	}, {
+		"name": "Utah Jazz",
+		"sport": "Basketball",
+		"city": "Utah",
+		"logo_url": "http://content.sportslogos.net/logos/6/234/full/6749_utah_jazz-primary-2017.png"
+	}, {
+		"name": "Washington Wizards",
+		"sport": "Basketball",
+		"city": "Washington",
+		"logo_url": "http://content.sportslogos.net/logos/6/219/full/5671_washington_wizards-primary-2016.png"
+	}
+
+]
