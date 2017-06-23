a4e38106473fc822c9582e88f5ce8552d65eceb5
Tim Chase <tchase4544@gmail.com>
2017-06-23 16:09:35 -0400

    nfl and nhl json data

---
+++ nfl.js
@@ -0,0 +1,191 @@
+let nfl = [{
+        "city": "Arizona",
+        "name": "Cardinals",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/177/full/kwth8f1cfa2sch5xhjjfaof90.png"
+    },
+    {
+        "city": "Atlanta",
+        "name": "Falcons",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/173/full/299.png"
+    }, {
+        "city": "Baltimore",
+        "name": "Ravens",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/153/full/318.png"
+    }, {
+        "city": "Buffalo",
+        "name": "Bills",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/149/full/n0fd1z6xmhigb0eej3323ebwq.png"
+    },
+    {
+        "city": "Carolina",
+        "name": "Panthers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/174/full/f1wggq2k8ql88fe33jzhw641u.png"
+    },
+    {
+        "city": "Cincinati",
+        "name": "Bengals",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/154/full/403.gif"
+    },
+    {
+        "city": "Chicago",
+        "name": "Bears",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/169/full/364.png"
+    },
+    {
+        "city": "Cleveland",
+        "name": "Browns",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/155/full/7855_cleveland_browns-primary-2015.png"
+    },
+    {
+        "city": "Dallas",
+        "name": "Cowboys",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/165/full/406.gif"
+    },
+    {
+        "city": "Denver",
+        "name": "Broncos",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/161/full/9ebzja2zfeigaziee8y605aqp.png"
+    },
+    {
+        "city": "Detroit",
+        "name": "Lions",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/170/full/1398_detroit_lions-primary-2017.png"
+    },
+    {
+        "city": "Green Bay",
+        "name": "Packers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/171/full/dcy03myfhffbki5d7il3.png"
+    },
+    {
+        "city": "Houston",
+        "name": "Texans",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/157/full/570.png"
+    },
+    {
+        "city": "Indianapolis",
+        "name": "Colts",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/158/full/593.png"
+    },
+    {
+        "city": "Jacksonville",
+        "name": "Jaquars",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/159/full/8856_jacksonville_jaguars-alternate-2013.png"
+    },
+    {
+        "city": "Kansas City",
+        "name": "Chiefts",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/162/full/857.png"
+    },
+    {
+        "city": "Miami",
+        "name": "Dolphins",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/150/full/4105_miami_dolphins-primary-2013.png"
+    },
+    {
+        "city": "Minnesota",
+        "name": "Vikings",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/172/full/2704_minnesota_vikings-primary-2013.png"
+    },
+    {
+        "city": "New England",
+        "name": "Patriots",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/151/full/y71myf8mlwlk8lbgagh3fd5e0.gif"
+    },
+    {
+        "city": "New Orleans",
+        "name": "Saints",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/175/full/907.png"
+    },
+    {
+        "city": "New York",
+        "name": "Giants",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/166/full/919.gif"
+    },
+    {
+        "city": "New York",
+        "name": "Jets",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/152/full/v7tehkwthrwefgounvi7znf5k.gif"
+    },
+    {
+        "city": "Las Vagas",
+        "name": "Raiders",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/163/full/g9mgk6x3ge26t44cccm9oq1vl.gif"
+    },
+    {
+        "city": "Philidelphia",
+        "name": "Eagles",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/167/full/960.png"
+    },
+    {
+        "city": "Pittsburgh",
+        "name": "Steelers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/156/full/970.png"
+    },
+    {
+        "city": "Los Angeles",
+        "name": "Chargers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/6446/full/2415_los_angeles__chargers-primary-2017.png"
+    },
+    {
+        "city": "Seattle",
+        "name": "Seahawks",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/180/full/8587_seattle_seahawks-wordmark-2012.png"
+    },
+    {
+        "city": "San Francisco",
+        "name": "49ers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/179/full/9455_san_francisco_49ers-primary-2009.gif"
+    },
+    {
+        "city": "Los Angeles",
+        "name": "Rams",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/5941/full/5317_los_angeles_rams-primary-2016.png"
+    },
+    {
+        "city": "Tampa Bay",
+        "name": "Buccaneers",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/176/full/3670_tampa_bay_buccaneers-primary-2014.png"
+    },
+    {
+        "city": "Tennessee",
+        "name": "Titants",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/160/full/1053.png"
+    },
+    {
+        "city": "Washington",
+        "name": "Redskins",
+        "sport": "Football",
+        "logo_url": "http://content.sportslogos.net/logos/7/168/full/im5xz2q9bjbg44xep08bf5czq.gif"
+    }
+]
