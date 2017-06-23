99abb1f4219bae8029a94bb8f3606770812beed9
Tarik Kazanci <tarikkzc@gmail.com>
2017-06-23 16:14:54 -0400

    mls data completed

---
+++ mls.js
@@ -0,0 +1,133 @@
+let mlsData = [{
+		"name": "Chicago Fire",
+		"sport": "Soccer",
+		"city": "Chicago",
+		"logo_url": "http://content.sportslogos.net/logos/9/323/full/umsypmvh1fcxyefrqcfio0yfy.gif"
+	},
+	{
+		"name": "Colorado Rapids",
+		"sport": "Soccer",
+		"city": "Colorado",
+		"logo_url": "http://content.sportslogos.net/logos/9/328/full/rkjf0o1eifa47lh6uhy4oj5qy.gif"
+	},
+	{
+    "name": "D.C. United",
+		"sport": "Soccer",
+		"city": "Washington D.C",
+		"logo_url": "http://content.sportslogos.net/logos/9/325/full/9001_dc_united-primary-2016.png"
+	},
+	{
+    "name": "FC Dallas",
+		"sport": "Soccer",
+		"city": "Dallas",
+		"logo_url": "http://content.sportslogos.net/logos/9/329/full/97yz6vcggd25lhyswy2fgvhfz.gif"
+	},
+	{
+    "name": "Houston Dynamo",
+		"sport": "Soccer",
+		"city": "Houston",
+		"logo_url": "http://content.sportslogos.net/logos/9/1482/full/h8ga63uac257c37js2m47hqke.gif"
+	},
+	{
+    "name": "Montreal Impact",
+		"sport": "Soccer",
+		"city": "Montreal",
+		"logo_url": "http://content.sportslogos.net/logos/9/2837/full/ugsnhv0oteegw8sjs3e4wivvg.gif"
+	},
+	{
+    "name": "LA Galaxy",
+		"sport": "Soccer",
+		"city": "Los Angeles",
+		"logo_url": "http://content.sportslogos.net/logos/9/331/full/wcwwzw1ysgmdcgkguwof.png"
+	},
+	{
+    "name": "New England Revolution",
+		"sport": "Soccer",
+		"city": "New England",
+		"logo_url": "http://content.sportslogos.net/logos/9/327/full/qtfoxrlfgs3gjvif4ao608tia.gif"
+	},
+	{
+    "name": "New York City FC",
+		"sport": "Soccer",
+		"city": "New York City",
+		"logo_url": "http://content.sportslogos.net/logos/9/5119/full/6000_new_york_city__fc-primary-2015.png"
+	},
+	{
+    "name": "New York Red Bulls",
+		"sport": "Soccer",
+		"city": "New York City",
+		"logo_url": "http://content.sportslogos.net/logos/9/1876/full/i9ni847kriagxdlb7xewa6dl8.gif"
+	},
+	{
+    "name": "Orlando City SC",
+		"sport": "Soccer",
+		"city": "Orlando",
+		"logo_url": "http://content.sportslogos.net/logos/9/5135/full/6486_orlando_city__sc-primary-2015.png"
+	},
+	{
+    "name": "Philadelphia Union",
+		"sport": "Soccer",
+		"city": "Philadelphia",
+		"logo_url": "http://content.sportslogos.net/logos/9/2761/full/qee7soq7augbn2jofb315sw1i.gif"
+	},
+	{
+    "name": "Portland Timbers",
+		"sport": "Soccer",
+		"city": "Portland",
+		"logo_url": "http://content.sportslogos.net/logos/9/2836/full/9842_portland__timbers-primary-2015.png"
+	},
+	{
+    "name": "Real Salt Lake",
+		"sport": "Soccer",
+		"city": "Salt Lake City",
+		"logo_url": "http://content.sportslogos.net/logos/9/394/full/4122__real_salt_lake-primary-2010.png"
+	},
+	{
+    "name": "San Jose Earthquakes",
+		"sport": "Soccer",
+		"city": "San Jose",
+		"logo_url": "http://content.sportslogos.net/logos/9/332/full/7952_san_jose_earthquakes-primary-2014.png"
+	},
+	{
+    "name": "Seattle Sounders FC",
+		"sport": "Soccer",
+		"city": "Seattle",
+		"logo_url": "http://content.sportslogos.net/logos/9/2760/full/2xkgg8fsj5zgywdhx5in779ed.gif"
+	},
+	{
+    "name": "Sporting Kansas City",
+		"sport": "Soccer",
+		"city": "Kansas City",
+		"logo_url": "http://content.sportslogos.net/logos/9/2834/full/hzgsk662crxwpeffygd9vazda.png"
+	},
+	{
+    "name": "Toronto FC",
+		"sport": "Soccer",
+		"city": "Toronto",
+		"logo_url": "http://content.sportslogos.net/logos/9/1249/full/0gfaomjubvw58whdep6w.gif"
+	},
+	{
+    "name": "Vancouver Whitecaps",
+		"sport": "Soccer",
+		"city": "Vancouver",
+		"logo_url": "http://content.sportslogos.net/logos/9/2835/full/vk6eh75qeug9nejz5g4k1evlo.gif"
+	},
+  {
+    "name": "Atlanta United",
+		"sport": "Soccer",
+		"city": "Atlanta",
+		"logo_url": "http://content.sportslogos.net/logos/9/5475/full/7393_atlanta_united_fc-primary-2017.png"
+	},
+  {
+    "name": "Minnesota United",
+		"sport": "Soccer",
+		"city": "Minnesota",
+		"logo_url": "http://content.sportslogos.net/logos/9/6288/full/2759_minnesota__united_fc-primary-2017.png"
+	},
+  {
+    "name": "Los Angeles Football Club",
+		"sport": "Soccer",
+		"city": "Los Angeles",
+		"logo_url": "http://content.sportslogos.net/logos/9/5480/full/5524_lafc_-primary-2018.png"
+	}
+]
