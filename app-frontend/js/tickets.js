d37b657804710d71e22d46a2c007d088e4e4e7b0
Tim Chase <tchase4544@gmail.com>
2017-06-27 16:24:37 -0400

    tickets and weather

--- app-frontend/js/tickets.js
+++ app-frontend/js/tickets.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 		city = $('#city').text()
 		name = $('#name').text()
 		// 
-		console.log(city +" : "+ name)
+		// console.log(city +" : "+ name)
 	}
 
 	function delayGetNames(){
@@ -48,14 +48,35 @@ $(document).ready(function () {
   		// console.log("i was clicked")
   		teamName = $('#teamName').text()
   		venue = $('#venueName').text()
+
+  		venue = format(venue)
+
   		$.getJSON("https://app.ticketmaster.com/discovery/v2/events.json?apikey=&keyword="+venue+" "+teamName,function(data){
   			// console.log(data._embedded.events)
   			let venueData = data._embedded.events
 
   			for (let i = 0; i < venueData.length; i++) {
-  				let games = `<div class="events"><a href="${venueData[i].url}"><p>${venueData[i].name}</p><p>${venueData[i].dates.start.localDate} @ ${venueData[i].dates.start.localTime}</p></a></div>`
+  				let vgames = `<div class="events"><a href="${venueData[i].url}"><p>${venueData[i].name}</p><p>${venueData[i].dates.start.localDate} @ ${venueData[i].dates.start.localTime}</p></a></div>`
 
-  				venueEvents.append(games)
+  				venueEvents.append(vgames)
+  			}
+  		})
+  		let address = $('#adr').text()
+		let num = address.length - 5
+		let zip = address.slice(num)
+  		$.getJSON("http://api.wunderground.com/api//forecast10day/q/"+zip+".json",function(data){
+  			let weather = data.forecast.simpleforecast.forecastday
+  			console.log(weather)
+
+  			for (let i = 0; i < weather.length; i++) {
+  				let days = `<div class="weatherDays">
+  								<p>${weather[i].conditions}</p>
+  								<img src="${weather[i].icon_url}" style="width:100%;" />
+  								<p>High: ${weather[i].high.fahrenheit}, Low: ${weather[i].low.fahrenheit}</p>
+  								<p>${weather[i].date.weekday}</p>
+  							</div>`
+
+  				$('#weatherReport').append(days)
   			}
   		})
 
@@ -64,7 +85,28 @@ $(document).ready(function () {
   	delayGetNames()
   	delayData()
 
-	
-
 })
 })()
+
+
+// adr = $('#adr').text()
+// => "601 F St NW, Washington, DC 20004"
+
+// num = adr.length -5
+// =>28
+
+// zip = adr.slice(num)
+// =>"20004"
+
+
+
+
+
+
+
+
+
+
+
+
+
