522f089f2c830feec0d7a247da2fc402574aad17
superbuggy <jpqreichard@gmail.com>
2017-07-20 11:20:28 -0400

    add comments

--- app-frontend/js/tickets.js
+++ app-frontend/js/tickets.js
@@ -1,98 +1,118 @@
+// Mixing jquery and angularjs for this type of thing is awkward/hacky
+// ultimately, you should move this out into a factory so that AngularJS's
+// event loop is 'automagically' aware of changes in the View Model, or the data
+// that would otherwise be in the ViewModel. It would also take less code to
+// accomplish the same result with less pain. I assume that you tried that
+// approach initially and resorted to this approach for technical reasons. it
+// is always a good idea to document that in the comments.
 
 (function(){
-$(document).ready(function () {
+	$(document).ready(function () {
 
-	let ticketPlace = $('#ticketsShow')
-	let venueEvents = $('#venueEvents')
-	let stadium = $('#venueButton')
-	let city = "";
-	let name = "";
-	let venue = "";
-	let teamName = ""
+		let ticketPlace = $('#ticketsShow')
+		let venueEvents = $('#venueEvents')
+		let stadium = $('#venueButton')
+		let city = "";
+		let name = "";
+		let venue = "";
+		let teamName = ""
 
-	function getNames(){
-		city = $('#city').text()
-		name = $('#name').text()
+		function getNames(){
+			city = $('#city').text()
+			name = $('#name').text()
 
-		// 
+			//
 
-		// console.log(city +" : "+ name)
-	}
+			// console.log(city +" : "+ name)
+		}
 
-	function delayGetNames(){
-  		setTimeout(getNames, 100);
-  	}
+		// This is pretty hacky, and may not work as intended if angular renders the templates slower than expected, like with an older machine, or a phone low on battery life with lots of apps open. the setTimeout here is being used since jQ's doc ready fires before angularjs renders html
 
-  	function delayData(){
-  		setTimeout(apiCall, 1300);
-  	}
+		function delayGetNames(){
+			setTimeout(getNames, 100);
+		}
 
-  	function format(string) {
+		function delayData(){
+			setTimeout(apiCall, 1300);
+		}
 
-  		return string.split("&").join(" ")
-  	}
+		function format(string) {
 
-  	function apiCall(){
-  		$.getJSON("https://app.ticketmaster.com/discovery/v2/events.json?apikey=HGIa2FOFAVlS380zbmEhDIz76AJKXWVA&keyword="+city+" "+name,function(data){
+			return string.split("&").join(" ")
+		}
 
-  			let ticketData = data._embedded.events
+		function apiCall(){
+			// in terms of adapting this to a factory, it would take 2 params, :city and :name
+			// another tricky aspect though is how the handle the api key
+			// you want to avoid committing keys, of course, but it probably wasn't
+			// what to do in a situation like this. it might make sense to have the
+			// api key at a special endpoint on the server where it can actually be
+			// stored in an environment variable.
 
-  			console.log(ticketData)
+			// you could also make this call on the backend itself with something like
+			// HTTParty and have the data on your rails server as well for consumption
+			// by your angular front-end
+			$.getJSON("https://app.ticketmaster.com/discovery/v2/events.json?apikey=HGIa2FOFAVlS380zbmEhDIz76AJKXWVA&keyword=" + city + " " + name, function (data) {
 
-  			for (let i = 0; i < ticketData.length; i++) {
-  				let games = `<div class="events"><a href="${ticketData[i].url}" target="_blank" ><p>${ticketData[i].name}</p><p>${ticketData[i].dates.start.localDate} @ ${ticketData[i].dates.start.localTime}</p></a></div>`
+				let ticketData = data._embedded.events
 
-  				ticketPlace.append(games)
-  			}
-  		})
-  	}
+				console.log(ticketData)
+				// forEach
+				for (let i = 0; i < ticketData.length; i++) {
+					let games = `<div class="events"><a href="${ticketData[i].url}" target="_blank" ><p>${ticketData[i].name}</p><p>${ticketData[i].dates.start.localDate} @ ${ticketData[i].dates.start.localTime}</p></a></div>`
 
-  	stadium.click(function(e) {
-  		e.preventDefault()
-  		// console.log("i was clicked")
-  		teamName = $('#teamName').text()
-  		venue = $('#venueName').text()
+					ticketPlace.append(games)
+				}
+			})
+		}
 
-  		venue = format(venue)
+		stadium.click(function(e) {
+			e.preventDefault()
+			// console.log("i was clicked")
+			teamName = $('#teamName').text()
+			venue = $('#venueName').text()
 
+			venue = format(venue)
 
-  		$.getJSON("https://app.ticketmaster.com/discovery/v2/events.json?apikey=HGIa2FOFAVlS380zbmEhDIz76AJKXWVA&keyword="+venue+" "+teamName,function(data){
+			// I'd have a factory for communicating with the
+			$.getJSON("https://app.ticketmaster.com/discovery/v2/events.json?apikey=HGIa2FOFAVlS380zbmEhDIz76AJKXWVA&keyword="+venue+" "+teamName,function(data){
 
-  			// console.log(data._embedded.events)
-  			let venueData = data._embedded.events
+				// console.log(data._embedded.events)
+				let venueData = data._embedded.events
+				//use forEach
+				for (let i = 0; i < venueData.length; i++) {
+					let vgames = `<div class="events"><a href="${venueData[i].url}" target="_blank" ><p>${venueData[i].name}</p><p>${venueData[i].dates.start.localDate} @ ${venueData[i].dates.start.localTime}</p></a></div>`
 
-  			for (let i = 0; i < venueData.length; i++) {
-  				let vgames = `<div class="events"><a href="${venueData[i].url}" target="_blank" ><p>${venueData[i].name}</p><p>${venueData[i].dates.start.localDate} @ ${venueData[i].dates.start.localTime}</p></a></div>`
+					venueEvents.append(vgames)
+				}
+			})
+			let address = $('#adr').text()
+			let num = address.length - 5
+			let zip = address.slice(num)
 
-  				venueEvents.append(vgames)
-  			}
-  		})
-  		let address = $('#adr').text()
-		let num = address.length - 5
-		let zip = address.slice(num)
+			$.getJSON("http://api.wunderground.com/api/d0f7b2301d8d8c2b/forecast10day/q/"+zip+".json",function(data){
 
-  		$.getJSON("http://api.wunderground.com/api/d0f7b2301d8d8c2b/forecast10day/q/"+zip+".json",function(data){
+				let weather = data.forecast.simpleforecast.forecastday
+				console.log(weather)
+				// use forEach
+				for (let i = 0; i < weather.length; i++) {
+					let days = `<div class="weatherDays">
 
-  			let weather = data.forecast.simpleforecast.forecastday
-  			console.log(weather)
+					<img src="${weather[i].icon_url}" style="width:100%;" />
+					<p>High: ${weather[i].high.fahrenheit}, Low: ${weather[i].low.fahrenheit}</p>
+					<p>${weather[i].date.weekday}</p>
+				</div>`
 
-  			for (let i = 0; i < weather.length; i++) {
-  				let days = `<div class="weatherDays">
-  								
-  								<img src="${weather[i].icon_url}" style="width:100%;" />
-  								<p>High: ${weather[i].high.fahrenheit}, Low: ${weather[i].low.fahrenheit}</p>
-  								<p>${weather[i].date.weekday}</p>
-  							</div>`
+				$('#weatherReport').append(days)
+			}
+		})
 
-  				$('#weatherReport').append(days)
-  			}
-  		})
+	})
 
-  	})
-
-  	delayGetNames()
-  	delayData()
+	// delayGetNames()
+	// delayData()
+	apiCall()
+	getNames()
 
 })
 })()
-
---
+++ feedback.md
@@ -0,0 +1,21 @@
+# Project 3 Evaluation
+
+## Back-end Technology
+**3: Excelling**
+>Includes many well-structured models, and advanced functionality such as authorization, 3rd-party API integration, or other technology not covered in class
+
+## Front-end Technology
+**2: Performing**
+>Front-end makes appropriate use of Angular controllers and states with ui-router.
+
+## Code Quality
+**3: Excelling**
+>No major code quality issues, makes use of Ruby best practices appropriately, and follows techniques such as separation of concerns, abstraction, and encapsulation
+
+## Style & Appearance
+**2: Performing**
+App had a professional appearance that made use of some aspects of responsive design.
+
+## Planning / Process / Submission
+**1: Progressing**
+>App is submitted, with basic evidence of planning. Documentation exists, but lacks common areas such as setup instructions, description of application functionality and link to deployed application
